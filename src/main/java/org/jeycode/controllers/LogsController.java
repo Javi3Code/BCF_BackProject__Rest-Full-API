28995dcc2ae1fa5243984e4748f492535ec5f023
@JeyCode <j.code.initialize@gmail.com>
2021-04-19 20:55:04 +0200

    Se crea servicio de envío de logs a soporte. Se limpia LogsController, de esta forma quedan limpios todos los controller a falta de incluir swagger.

--- src/main/java/org/jeycode/controllers/LogsController.java
+++ src/main/java/org/jeycode/controllers/LogsController.java
@@ -1,11 +1,7 @@
 package org.jeycode.controllers;
 
-import java.util.concurrent.ExecutionException;
-import java.util.function.BiConsumer;
-
 import org.jeycode.constants.ApplicationExceptionUtils;
-import org.jeycode.service.genericservice.FilesStorageService;
-import org.jeycode.service.genericservice.SendMailService;
+import org.jeycode.service.genericservice.SendLogsToSupportService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,56 +9,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Controller
 @RequiredArgsConstructor
-@RequestMapping("/files")
-@Slf4j
+@Controller
+@RequestMapping("/logs")
 public class LogsController implements ApplicationExceptionUtils
 {
 
-      private final FilesStorageService filesStorageService;
-      private final SendMailService sendMailService;
+      private final SendLogsToSupportService sendLogsToSupportService;
 
       @GetMapping
       @ResponseBody
       public boolean sendLogsToApplicationSupport(@RequestParam(required = false, defaultValue = LOGS_REQUEST_DEFAULT_MSG) String clientErrorMessage)
       {
-            String messageToSendWithLogs = clientErrorMessage.isBlank() ? LOGS_REQUEST_DEFAULT_MSG : clientErrorMessage;
-            BiConsumer<Boolean,Throwable> action = getAction(messageToSendWithLogs);
-            try
-            {
-                  return filesStorageService.prepareLogsPackage()
-                                            .whenCompleteAsync(action)
-                                            .get();
-            }
-            catch (InterruptedException | ExecutionException ex)
-            {
-                  return false;
-            }
-
-      }
-
-      private BiConsumer<Boolean,Throwable> getAction(String messageToSendWithLogs)
-      {
-            BiConsumer<Boolean,Throwable> action = (result,ex)->
-                  {
-                        if (result)
-                        {
-                              sendMailService.sendLogsToApplicationSupport(messageToSendWithLogs,filesStorageService.getFileToSend())
-                                             .whenCompleteAsync((actionResult,exR)->
-                                                   {
-                                                         if (actionResult)
-                                                         {
-                                                               log.info("Se envió el correo a soporte correctamente.");
-                                                               filesStorageService.deleteTempDir();
-                                                               log.info("Se borraron los directorios temporales para los logs.");
-                                                         }
-                                                   });
-                        }
-                  };
-            return action;
+            return sendLogsToSupportService.sendLogsToApplicationSupport(clientErrorMessage);
       }
 
 }
---
+++ src/main/java/org/jeycode/service/genericservice/SendLogsToSupportService.java
@@ -0,0 +1,62 @@
+package org.jeycode.service.genericservice;
+
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiConsumer;
+
+import org.jeycode.constants.ApplicationExceptionUtils;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SendLogsToSupportService implements ApplicationExceptionUtils
+{
+
+      private final FilesStorageService filesStorageService;
+      private final SendMailService sendMailService;
+
+      public boolean sendLogsToApplicationSupport(String clientErrorMessage)
+      {
+            String messageToSendWithLogs = clientErrorMessage.isBlank() ? LOGS_REQUEST_DEFAULT_MSG : clientErrorMessage;
+            BiConsumer<Boolean,Throwable> action = getAction(messageToSendWithLogs);
+            try
+            {
+                  return filesStorageService.prepareLogsPackage()
+                                            .whenCompleteAsync(action)
+                                            .get();
+            }
+            catch (InterruptedException | ExecutionException ex)
+            {
+                  return false;
+            }
+
+      }
+
+      /*
+       * *****************************Private Methods*********************************
+       */
+
+      private BiConsumer<Boolean,Throwable> getAction(String messageToSendWithLogs)
+      {
+            BiConsumer<Boolean,Throwable> action = (result,ex)->
+                  {
+                        if (result)
+                        {
+                              sendMailService.sendLogsToApplicationSupport(messageToSendWithLogs,filesStorageService.getFileToSend())
+                                             .whenCompleteAsync((actionResult,exR)->
+                                                   {
+                                                         if (actionResult)
+                                                         {
+                                                               log.info("Se envió el correo a soporte correctamente.");
+                                                               filesStorageService.deleteTempDir();
+                                                               log.info("Se borraron los directorios temporales para los logs.");
+                                                         }
+                                                   });
+                        }
+                  };
+            return action;
+      }
+}
